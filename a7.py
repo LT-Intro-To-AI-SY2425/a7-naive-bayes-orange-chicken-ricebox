6012576588ccfde95b8c9e1df0cf104df47dec9a
Jhimawan <Jhimawan@cps.edu>
2025-04-14 11:13:17 -0500

    empty commit

--- a7.py
+++ a7.py
@@ -66,12 +66,11 @@ class BayesClassifier:
         # enumerate function, which loops over something and has an automatic counter.
         # write something like this to track progress (note the `# type: ignore` comment
         # which tells mypy we know better and it shouldn't complain at us on this line):
-        for index, filename in enumerate(files, 1): # type: ignore
-            print(f"Training on file {index} of {len(files)}")
+        # for index, filename in enumerate(files, 1): # type: ignore
+        #     print(f"Training on file {index} of {len(files)}")
         #     <the rest of your code for updating frequencies here>
-            text = self.load_file(os.path.join(self.training_data_directory, filename))
-            tokens = self.tokenize(text)
-            
+
+
         # we want to fill pos_freqs and neg_freqs with the correct counts of words from
         # their respective reviews
         
@@ -82,10 +81,6 @@ class BayesClassifier:
         # positive frequency dictionary. If it is neither a postive or negative file,
         # ignore it and move to the next file (this is more just to be safe; we won't
         # test your code with neutral reviews)
-            if filename.startswith(self.pos_file_prefix):
-                self.update_dict(tokens, self.pos_freqs)
-            elif filename.startswith(self.neg_file_prefix):
-                self.update_dict(tokens, self.neg_freqs)
 
         # Updating frequences: to update the frequencies for each file, you need to get
         # the text of the file, tokenize it, then update the appropriate dictionary for
@@ -102,9 +97,7 @@ class BayesClassifier:
         # avoid extra work in the future (using the save_dict method). The objects you
         # are saving are self.pos_freqs and self.neg_freqs and the filepaths to save to
         # are self.pos_filename and self.neg_filename
-        self.save_dict(self.pos_freqs, self.pos_filename)
-        self.save_dict(self.neg_freqs, self.neg_filename)
-        
+
     def classify(self, text: str) -> str:
         """Classifies given text as positive, negative or neutral from calculating the
         most likely document class to which the target string belongs
@@ -228,25 +221,21 @@ class BayesClassifier:
             freqs - dictionary of frequencies to update
         """
         # TODO: your work here
-        for word in words:
-            if word in freqs:
-                freqs[word] += 1
-            else:
-                freqs[word] = 1
+        pass  # remove this line once you've implemented this method
 
 
 if __name__ == "__main__":
     # uncomment the below lines once you've implemented `train` & `classify`
-    b = BayesClassifier()
-    a_list_of_words = ["I", "really", "like", "this", "movie", ".", "I", "hope", \
-                       "you", "like", "it", "too"]
-    a_dictionary = {}
-    b.update_dict(a_list_of_words, a_dictionary)
-    assert a_dictionary["I"] == 2, "update_dict test 1"
-    assert a_dictionary["like"] == 2, "update_dict test 2"
-    assert a_dictionary["really"] == 1, "update_dict test 3"
-    assert a_dictionary["too"] == 1, "update_dict test 4"
-    print("update_dict tests passed.")
+    # b = BayesClassifier()
+    # a_list_of_words = ["I", "really", "like", "this", "movie", ".", "I", "hope", \
+    #                    "you", "like", "it", "too"]
+    # a_dictionary = {}
+    # b.update_dict(a_list_of_words, a_dictionary)
+    # assert a_dictionary["I"] == 2, "update_dict test 1"
+    # assert a_dictionary["like"] == 2, "update_dict test 2"
+    # assert a_dictionary["really"] == 1, "update_dict test 3"
+    # assert a_dictionary["too"] == 1, "update_dict test 4"
+    # print("update_dict tests passed.")
 
     # pos_denominator = sum(b.pos_freqs.values())
     # neg_denominator = sum(b.neg_freqs.values())
